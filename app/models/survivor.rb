5a0292449f4c2eb0a7c08fa70db1d421deae016a
diguifi <lumia-diguifi@hotmail.com>
2019-04-26 14:53:11 -0300

    Corrige model de survivor para deletar inventory em cascata

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,5 +1,5 @@
 class Survivor < ApplicationRecord
-    has_one :inventory
+    has_one :inventory, dependent: :destroy
     has_many :infection_reported, class_name: 'InfectionReport', foreign_key: 'survivor_reporter_id'
     has_many :infection_received, class_name: 'InfectionReport', foreign_key: 'survivor_reported_id'
 	accepts_nested_attributes_for :inventory
